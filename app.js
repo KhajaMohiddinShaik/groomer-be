4424513e7c0a41dc71a30070894e90e005f07e39
Mohammed shahid <84904302+mohammedshahid096@users.noreply.github.com>
2023-11-24 17:31:36 +0530

    updated app.js files

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+// app.use('/static', express.static(path.join(__dirname, '/src/static')))
 // for using static files
 app.use("/public", express.static(`${__dirname}/public`));
 
